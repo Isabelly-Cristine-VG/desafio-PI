b1c5592bf317dd348c3f55b5897c5666eb1dd8cb
MariliaToscano <marilia.araujo@sptech.school>
2025-06-17 12:52:47 -0300

    dadosDash

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function buscarUltimasMedidas(idAquario, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
+        luminosidade, 
         dht11_umidade as umidade,
                         momento,
                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
@@ -15,21 +15,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
 }
--- src/routes/medidas.js
+++ src/routes/medidas.js
@@ -7,8 +7,6 @@ router.get("/ultimas/:idAquario", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
+
 
 module.exports = router;
