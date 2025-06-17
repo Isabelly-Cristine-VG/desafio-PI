0605cf7762655ef7c3622e49f87f1e2b35397ad8
willianbsilvadev <willian.dsilva@sptech.school>
2025-06-17 13:46:03 -0300

    logica dash

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,7 +1,7 @@
 -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-drop database estufaTech;
+
 CREATE DATABASE estufaTech;
 
 USE estufaTech;
@@ -49,21 +49,44 @@ create table medida (
 	FOREIGN KEY (fk_estufa) REFERENCES estufa(id)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'A1B2C3');
 insert into estufa (descricao, fk_empresa) values ('Estufa A', 1);
 insert into estufa (descricao, fk_empresa) values ('Estufa B', 1);
 insert into estufa (descricao, fk_empresa) values ('Estufa C', 1);
 
 insert into medida(dht11_umidade,luminosidade,momento,fk_estufa)values
-(60.2,22.2,'2025-06-17 13:12:00',1),
-(62.8,22.2,'2025-06-17 13:12:05',1),
-(61.1,22.2,'2025-06-17 13:12:10',1),
-(66.7,22.2,'2025-06-17 13:12:15',1),
-(68.2,22.2,'2025-06-17 13:12:20',1);
+(60.2,21.3,'2025-06-17 13:12:00',1),
+(62.8,23.1,'2025-06-17 13:12:05',1),
+(61.1,21.3,'2025-06-17 13:12:10',1),
+(66.7,20.5,'2025-06-17 13:12:15',1),
+(68.2,21.5,'2025-06-17 13:12:20',1),
+(62.2,20.2,'2025-06-17 13:12:25',1),
+(64.8,21.2,'2025-06-17 13:12:30',1),
+(61.1,23.2,'2025-06-17 13:12:35',1),
+(61.7,22.2,'2025-06-17 13:12:40',1),
+(65.2,23.2,'2025-06-17 13:12:45',1);
 
 insert into medida(dht11_umidade,luminosidade,momento,fk_estufa)values
 (61.1,22.4,'2025-06-17 13:12:00',2),
 (62.4,23.6,'2025-06-17 13:12:05',2),
 (61.3,22.1,'2025-06-17 13:12:10',2),
 (65.2,21.9,'2025-06-17 13:12:15',2),
-(68.7,22.2,'2025-06-17 13:12:20',2);
+(68.7,22.2,'2025-06-17 13:12:20',2),
+(62.1,25.4,'2025-06-17 13:12:25',2),
+(61.4,23.6,'2025-06-17 13:12:30',2),
+(65.3,23.1,'2025-06-17 13:12:35',2),
+(62.2,22.9,'2025-06-17 13:12:40',2),
+(63.5,21.2,'2025-06-17 13:12:45',2);
+
+insert into medida(dht11_umidade,luminosidade,momento,fk_estufa)values
+(61.1,22.4,'2025-06-17 13:12:00',3),
+(62.4,23.6,'2025-06-17 13:12:05',3),
+(61.3,22.1,'2025-06-17 13:12:10',3),
+(65.2,21.9,'2025-06-17 13:12:15',3),
+(68.7,22.2,'2025-06-17 13:12:20',3),
+(64.1,22.2,'2025-06-17 13:12:25',3),
+(62.4,21.6,'2025-06-17 13:12:30',3),
+(67.3,22.1,'2025-06-17 13:12:35',3),
+(68.2,25.9,'2025-06-17 13:12:40',3),
+(65.7,23.2,'2025-06-17 13:12:45',3);
+
