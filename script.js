84c889c9a1aab2d04bd79133888ff79b3c21d802
MariliaToscano <marilia.araujo@sptech.school>
2025-06-17 11:15:44 -0300

    top5

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+
+const elementosSlider = document.querySelectorAll('.slider')
+const btProximo = document.getElementById('botaoProximo')
+const btAnterior = document.getElementById('botaoAnterior')
+
+let sliderAtual = 0
+
+function proximoSlider(){
+    esconderSlider()
+
+    if(sliderAtual === elementosSlider.length - 1 ){
+        sliderAtual = 0
+    }else{  
+        sliderAtual++
+    }
+
+    mostrarSlider()
+}
+
+function anteriorSlider(){
+    esconderSlider()
+
+    if(sliderAtual === 0){
+        sliderAtual = elementosSlider.length - 1
+    }else{  
+        sliderAtual--
+    }
+
+    mostrarSlider()
+}
+
+function esconderSlider(){
+    elementosSlider.forEach(item => item.classList.remove('on'));
+}
+
+function mostrarSlider(){
+    elementosSlider[sliderAtual].classList.add('on')
+}
+
+
+btProximo.addEventListener('click', proximoSlider)
+btAnterior.addEventListener('click', anteriorSlider)
+
+
+
 function corTipo(tipoOficial) {
     let tipo = tipoOficial[0]; // Considerando apenas o primeiro tipo
     let corzinhaLegal = '';
